42ea3bae3fa7c52477fb35c8a3975bb4f8b0164e
felipesty <felipestylex08@gmail.com>
2022-08-02 20:13:09 -0300

    req 1

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const talker = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+    const infos = await fs.readFile(talker, 'utf8');
+    res.status(200).json(JSON.parse(infos));
 });
 
 app.listen(PORT, () => {
